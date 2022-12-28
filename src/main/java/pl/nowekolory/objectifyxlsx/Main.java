4d8621bba33cf4b599b3f7ee09e8cb1a645d34af
Michał Piątkowski <kontakt@michalpiatkowski.pl>
2022-12-28 15:35:33 +0100

    Initial commit

---
+++ src/main/java/pl/nowekolory/objectifyxlsx/Main.java
@@ -0,0 +1,5 @@
+package pl.nowekolory.objectifyxlsx;
+
+public class Main {
+	
+}
---
+++ src/test/java/pl/nowekolory/objectifyxlsx/MainTest.java
@@ -0,0 +1,8 @@
+package pl.nowekolory.objectifyxlsx;
+
+public class MainTest {
+	
+    public static void main(String[] args) {
+        
+    }
+}
