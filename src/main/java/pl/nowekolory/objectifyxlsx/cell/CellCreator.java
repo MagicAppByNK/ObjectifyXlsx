e5f452de3b041e02b6b923c14cb1c425f75d5b17
Michał Piątkowski <kontakt@michalpiatkowski.pl>
2023-02-06 22:56:48 +0100

    AddCellXX is now overloaded function.

--- src/main/java/pl/nowekolory/objectifyxlsx/cell/CellCreator.java
+++ src/main/java/pl/nowekolory/objectifyxlsx/cell/CellCreator.java
@@ -27,19 +27,21 @@ public class CellCreator{
             return;
         }
         if(value instanceof String){
-            addCellWithString(row, (String) value, cellIndex);
+            addCell(row, (String) value, cellIndex);
         }else if(value instanceof Integer){
-            addCellWithInteger(row, (Integer) value, cellIndex);
+            addCell(row, (Integer) value, cellIndex);
         }else if(value instanceof Double){
-            addCellWithDouble(row, (Double) value, cellIndex);
+            addCell(row, (Double) value, cellIndex);
         }else if(value instanceof Long){
-            addCellWithLong(row, (Long) value, cellIndex);
+            addCell(row, (Long) value, cellIndex);
         }else if(value instanceof LocalDateTime){
-            addCellWithLocalDateTime(dateTimeStyle, row, (LocalDateTime) value, cellIndex);
+            addCell(dateTimeStyle, row, (LocalDateTime) value, cellIndex);
         }else if(value instanceof LocalDate){
-            addCellWithLocalDate(dateStyle, row, (LocalDate) value, cellIndex);
+            addCell(dateStyle, row, (LocalDate) value, cellIndex);
         }else if(value instanceof Boolean){
-            addCellWithBoolean(row, (Boolean) value, cellIndex);
+            addCell(row, (Boolean) value, cellIndex);
+        }else if(value instanceof Float){
+            addCell(row, (Float) value, cellIndex);
         }else{
             logger.error("No cell creator for given value class: {}", value.getClass());
         }
@@ -48,36 +50,40 @@ public class CellCreator{
     private void addEmptyCell(Row row, Integer cellIndex){
         row.createCell(cellIndex).setBlank();
     }
-    private void addCellWithString(Row row, String value, Integer cellIndex){
+    private void addCell(Row row, String value, Integer cellIndex){
         row.createCell(cellIndex).setCellValue(value);
     }
 
-    private void addCellWithInteger(Row row, Integer value, Integer cellIndex){
+    private void addCell(Row row, Integer value, Integer cellIndex){
         row.createCell(cellIndex).setCellValue(value);
     }
 
-    private void addCellWithDouble(Row row, Double value, Integer cellIndex){
+    private void addCell(Row row, Double value, Integer cellIndex){
         row.createCell(cellIndex).setCellValue(value);
     }
 
-    private void addCellWithLong(Row row, Long value, Integer cellIndex){
+    private void addCell(Row row, Long value, Integer cellIndex){
         row.createCell(cellIndex).setCellValue(value);
     }
 
-    private void addCellWithLocalDateTime(CellStyle cellStyle, Row row, LocalDateTime value, Integer cellIndex){
+    private void addCell(Row row, Float value, Integer cellIndex){
+        row.createCell(cellIndex).setCellValue(value);
+    }
+
+    private void addCell(CellStyle cellStyle, Row row, LocalDateTime value, Integer cellIndex){
         var cell = row.createCell(cellIndex);
         cell.setCellValue(value);
         cell.setCellStyle(cellStyle);
     }
 
-    private void addCellWithBoolean(Row row, Boolean value, Integer cellIndex){
+    private void addCell(Row row, Boolean value, Integer cellIndex){
         if(value == null){
             value = false;
         }
         row.createCell(cellIndex).setCellValue(value);
     }
 
-    private void addCellWithLocalDate(CellStyle cellStyle, Row row, LocalDate value, Integer cellIndex){
+    private void addCell(CellStyle cellStyle, Row row, LocalDate value, Integer cellIndex){
         var cell = row.createCell(cellIndex);
         cell.setCellValue(value);
         cell.setCellStyle(cellStyle);
